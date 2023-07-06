9eee5e3603bee587d29a26358809a39094b07aa9
Daniela Petruzalek <daniela.petruzalek@gmail.com>
2023-07-06 22:54:58 +0100

    improve test coverage

--- gildedrose/gildedrose_test.go
+++ gildedrose/gildedrose_test.go
@@ -21,6 +21,7 @@ func Test_NormalItem(t *testing.T) {
 }
 
 func Test_NormalItems(t *testing.T) {
+	name := "normal item"
 
 	tbl := []struct {
 		name     string
@@ -29,23 +30,151 @@ func Test_NormalItems(t *testing.T) {
 	}{
 		{
 			"normal item: q > 0, s > 0",
-			gildedrose.Item{"normal item", 10, 10},
-			gildedrose.Item{"normal item", 9, 9},
+			gildedrose.Item{name, 10, 10},
+			gildedrose.Item{name, 9, 9},
 		},
 		{
 			"normal item: q = 0, s > 0",
-			gildedrose.Item{"normal item", 10, 0},
-			gildedrose.Item{"normal item", 9, 0},
+			gildedrose.Item{name, 10, 0},
+			gildedrose.Item{name, 9, 0},
 		},
 		{
 			"normal item: q > 0, s < 0",
-			gildedrose.Item{"normal item", -1, 10},
-			gildedrose.Item{"normal item", -2, 8},
+			gildedrose.Item{name, -1, 10},
+			gildedrose.Item{name, -2, 8},
 		},
 		{
 			"normal item: q > 0, s = 0",
-			gildedrose.Item{"normal item", 0, 10},
-			gildedrose.Item{"normal item", -1, 8},
+			gildedrose.Item{name, 0, 10},
+			gildedrose.Item{name, -1, 8},
+		},
+	}
+
+	for _, tc := range tbl {
+		t.Run(tc.name, func(t *testing.T) {
+
+			items := []*gildedrose.Item{
+				&tc.input,
+			}
+
+			gildedrose.UpdateQuality(items)
+
+			if *items[0] != tc.expected {
+				t.Errorf("Expected %v but got %v ", tc.expected, *items[0])
+			}
+		})
+	}
+}
+
+func Test_AgedBrie(t *testing.T) {
+	name := "Aged Brie"
+
+	tbl := []struct {
+		name     string
+		input    gildedrose.Item
+		expected gildedrose.Item
+	}{
+		{
+			"aged brie: q > 0, s > 0",
+			gildedrose.Item{name, 10, 10},
+			gildedrose.Item{name, 9, 11},
+		},
+		{
+			"aged brie: q = 0, s > 0",
+			gildedrose.Item{name, 10, 0},
+			gildedrose.Item{name, 9, 1},
+		},
+		{
+			"aged brie: q > 0, s < 0",
+			gildedrose.Item{name, -1, 10},
+			gildedrose.Item{name, -2, 12},
+		},
+		{
+			"aged brie: q > 0, s = 0",
+			gildedrose.Item{name, 0, 10},
+			gildedrose.Item{name, -1, 12},
+		},
+	}
+
+	for _, tc := range tbl {
+		t.Run(tc.name, func(t *testing.T) {
+
+			items := []*gildedrose.Item{
+				&tc.input,
+			}
+
+			gildedrose.UpdateQuality(items)
+
+			if *items[0] != tc.expected {
+				t.Errorf("Expected %v but got %v ", tc.expected, *items[0])
+			}
+		})
+	}
+}
+
+func Test_BackstagePass(t *testing.T) {
+	name := "Backstage passes to a TAFKAL80ETC concert"
+
+	tbl := []struct {
+		name     string
+		input    gildedrose.Item
+		expected gildedrose.Item
+	}{
+		{
+			"backstage pass: q > 0, s > 0",
+			gildedrose.Item{name, 10, 10},
+			gildedrose.Item{name, 9, 12},
+		},
+		{
+			"backstage pass: q = 0, s > 0",
+			gildedrose.Item{name, 10, 0},
+			gildedrose.Item{name, 9, 2},
+		},
+		{
+			"backstage pass: q > 0, s < 0",
+			gildedrose.Item{name, -1, 10},
+			gildedrose.Item{name, -2, 0},
+		},
+		{
+			"backstage pass: q > 0, s = 0",
+			gildedrose.Item{name, 0, 10},
+			gildedrose.Item{name, -1, 0},
+		},
+	}
+
+	for _, tc := range tbl {
+		t.Run(tc.name, func(t *testing.T) {
+
+			items := []*gildedrose.Item{
+				&tc.input,
+			}
+
+			gildedrose.UpdateQuality(items)
+
+			if *items[0] != tc.expected {
+				t.Errorf("Expected %v but got %v ", tc.expected, *items[0])
+			}
+		})
+	}
+}
+
+func Test_Sulfuras(t *testing.T) {
+	name := "Sulfuras, Hand of Ragnaros"
+
+	tbl := []struct {
+		name     string
+		input    gildedrose.Item
+		expected gildedrose.Item
+	}{
+		{
+			"sulfuras",
+			gildedrose.Item{name, 0, 80},
+			gildedrose.Item{name, 0, 80},
+		},
+		{
+			"sulfuras s<0",
+			gildedrose.Item{name, -1, 80},
+			gildedrose.Item{name, -1, 80},
 		},
 	}
 
